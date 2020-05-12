bd0042adc186bca645c86581c80323cbe448d2ca
davenportnw <davenportnw@gmail.com>
2020-05-12 18:26:03 -0500

    Add countdown timer

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 
 
+//* Timer *//
+
+var timeEl = document.getElementById("timer");
+
+var secondsLeft = 60;
+
+function setTime(){
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds left.";
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+
+    }, 1000);
+}
+
+
+setTime();
+
